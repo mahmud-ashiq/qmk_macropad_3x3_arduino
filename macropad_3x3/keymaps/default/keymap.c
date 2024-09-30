5828b3ceb5d3a6f98e85809b61cb8c6ac23d16e7
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-09-30 22:30:04 +0600

    updated

--- macropad_3x3/keymaps/default/keymap.c
+++ macropad_3x3/keymaps/default/keymap.c
@@ -7,28 +7,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_l1] = LAYOUT_3x3(   
-        KC_BRID, KC_BRIU, KC_MUTE, 
-        KC_CAPS, KC_SCRL, KC_PSCR, 
-        KC_CALC, KC_MYCM, OSL(1)),
+      RCS(KC_ESC),  TG(1),      KC_MUTE,    
+      KC_WHOM,      KC_MAIL,    KC_MYCM,    
+      LCTL(KC_C),   LCTL(KC_V), KC_CALC),
     
     [_l2] = LAYOUT_3x3(
-        KC_VOLD, KC_VOLU, KC_MUTE, 
-        KC_CAPS, KC_SCRL, KC_PSCR, 
-        KC_CALC, KC_MYCM, OSL(1))
-    
+      RCS(KC_ESC),  KC_TRNS,        KC_NO,
+      RCS(KC_N),    LCA(KC_Z),      MEH(KC_W),
+      LCTL(KC_LBRC),LCTL(KC_RBRC),  LCTL(KC_0))
 
 };
-
-/*bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { 
-        if (clockwise) {
-            tap_code(KC_AUDIO_VOL_UP);
-        } else {
-            tap_code(KC_AUDIO_VOL_DOWN);
-        }
-    }
-    return false;
-}*/
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
   if (!encoder_update_user(index, clockwise)) return false;
@@ -37,18 +25,18 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     case _l1:        //if you're on layer 0
         if (index == 0) {
             if (clockwise) {
-            tap_code(KC_AUDIO_VOL_UP);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_AUDIO_VOL_DOWN);
+            tap_code(KC_VOLD);
         }
         }
         break;
     case _l2:        //if you're on layer 1
         if (index == 0) {
             if (clockwise) {
-            tap_code(KC_BRIU);
+            tap_code(MS_WHLR);
         } else {
-            tap_code(KC_BRID);
+            tap_code(MS_WHLL);
         }
         }
         break;
@@ -59,21 +47,13 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
 
 void keyboard_pre_init_user(void) {
 setPinOutput(B6);  
-setPinOutput(B2);  
 }
-
 layer_state_t layer_state_set_user(layer_state_t state) {
-    if(IS_LAYER_ON_STATE(state, 0)) { 
+    if(IS_LAYER_ON_STATE(state, 1)) { 
         writePinHigh(B6); 
     } 
     else { 
         writePinLow(B6);
-    } 
-    if(IS_LAYER_ON_STATE(state, 1)) { 
-        writePinHigh(B2); 
-    } 
-    else { 
-        writePinLow(B2); 
     } 
 return state; 
 }
